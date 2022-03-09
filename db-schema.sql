83c4f78a96f23c996d9948f527149d767ead8891
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2022-03-08 16:38:27 -0800

    initial

--- db-schema.sql
+++ db-schema.sql
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS ordered_products (
     order_id INT REFERENCES orders(o_id) NOT NULL ,
     product_id UUID REFERENCES products(p_id) NOT NULL,
     p_quantity INTEGER NOT NULL,
-    created_in TIMESTAMP NOT NULL DEFAULT NOW ()
+    created_in DATE NOT NULL DEFAULT NOW ()
 );
 DROP TABLE order_products;
 
