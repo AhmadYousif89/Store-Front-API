8ac9a58321133c8db0cef2f5064ffc120ff8c857
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2022-04-20 04:41:29 +0200

    code refactor

--- frontend/src/pages/Orders.tsx
+++ frontend/src/pages/Orders.tsx
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import { useSelector, RootStateOrAny, useDispatch } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import OrderItem from "../components/OrderItems";
 import Spinner from "../components/Spinner";
 import { getOrders } from "../features/orders/orderSlice";
+import "./styles/Orders.css";
 
 function Orders() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  // const orders = JSON.parse(localStorage.getItem("orders") as any);
   const { user } = useSelector((state: RootStateOrAny) => state.auth);
   const { orders, isLoading, isError, message } = useSelector(
     (state: RootStateOrAny) => state.orders,
@@ -21,28 +24,30 @@ function Orders() {
       toast.error("Access denied");
     }
     if (isError) toast.error(message);
-    dispatch(getOrders());
+    dispatch(getOrders(user.data.user_id));
   }, [user, isError, message, navigate, dispatch]);
 
   if (isLoading) return <Spinner />;
 
   return (
     <>
-      <section className="heading">
-        <h1>My Orders</h1>
-      </section>
-      <section className="content">
-        {orders.length > 0 ? (
-          <OrderItem orders={orders} />
-        ) : (
-          <h3>You don't have any orders...</h3>
-        )}
-      </section>
-      <Link className="flex" to={user ? `/dashboard/${user.data.name}` : ""}>
-        <div className="go_back">
+      <div className="container">
+        <section className="heading">
+          <h1>My Orders</h1>
+        </section>
+        <section className="content">
+          {orders.length > 0 ? (
+            <OrderItem orders={orders} />
+          ) : (
+            <h3>You don't have any orders...</h3>
+          )}
+        </section>
+        <div
+          className="go_back"
+          onClick={() => navigate(`/dashboard/${user.data.name}`)}>
           <FaAngleLeft /> Go Back <FaAngleRight />
         </div>
-      </Link>
+      </div>
     </>
   );
 }
